8e69ac94bb7854d73020b9243a314d1c3854f5c6
fuzionu <fuzionu@gmail.com>
2020-01-26 23:24:43 +0100

    initial commit

---
+++ src/Champions.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Champions
+{
+    public static void main(String[] args)
+    {
+        JFrame championsFrame = new JFrame("Champions");
+        JLabel ButtonsLabel = new JLabel();
+        JButton searchButton = new JButton();
+        JButton clearButton = new JButton();
+        JButton exitButton = new JButton();
+
+        searchButton.setText("Search");
+        clearButton.setText("Clear");
+        exitButton.setText("Exit");
+
+        ButtonsLabel.setLayout(new FlowLayout());
+        ButtonsLabel.add(searchButton);
+        ButtonsLabel.add(clearButton);
+        ButtonsLabel.add(exitButton);
+
+
+        championsFrame.add(ButtonsLabel);
+        championsFrame.setSize(500, 700);
+        championsFrame.setLocationRelativeTo(null);
+        championsFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        championsFrame.setVisible(true);
+    }
+}
---
+++ src/ChampionsLogic.java
@@ -0,0 +1,4 @@
+public class ChampionsLogic
+{
+    //code for champions logic
+}
