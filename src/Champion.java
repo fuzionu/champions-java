62526e641daafb49f593aef962d7a4b9efd04c97
fuzionu <fuzionu@gmail.com>
2020-01-30 21:22:17 +0100

    added class Champion and method CreateChampion

---
+++ src/Champion.java
@@ -0,0 +1,16 @@
+public class Champion
+{
+    String name;
+    String role;
+    int health;
+    String resourceType;
+    int resourceRegen;
+    int abilityPower;
+    String attackType;
+    String attackDamage;
+    float attackSpeed;
+    int attackRange;
+    int armor;
+    int magicResist;
+    int movementSpeed;
+}
--- src/ChampionsLogic.java
+++ src/ChampionsLogic.java
@@ -1,4 +1,25 @@
 public class ChampionsLogic
 {
-    //code for champions logic
+    public static Champion CreateChampion(String name, String role, int health, String resourceType, int resourceRegen,
+            int abilityPower, String attackType, String attackDamage, float attackSpeed, int attackRange, int armor,
+            int magicResist, int movementSpeed)
+    {
+        Champion champion = new Champion();
+
+        champion.name = name;
+        champion.role = role;
+        champion.health = health;
+        champion.resourceType = resourceType;
+        champion.resourceRegen = resourceRegen;
+        champion.abilityPower = abilityPower;
+        champion.attackType = attackType;
+        champion.attackDamage = attackDamage;
+        champion.attackSpeed = attackSpeed;
+        champion.attackRange = attackRange;
+        champion.armor = armor;
+        champion.magicResist = magicResist;
+        champion.movementSpeed = movementSpeed;
+
+        return champion;
+    }
 }
