1e469ed6a1afcc3a48c70aa1e3288022b40a7bc3
fuzionu <fuzionu@gmail.com>
2020-02-02 14:34:43 +0100

    removed GUI reference in Main, added tester for checking champs

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        GUI.GraphicUserInterface();
+        Scanner scanner = new Scanner(System.in);
+        boolean doContinue = true;
+
+        do
+        {
+            System.out.println("Type \"exit\" to close the program.");
+            System.out.print("Enter champ name: ");
+            String champName = scanner.nextLine();
+
+            if (champName.equals("exit"))
+            {
+                doContinue = false;
+            }
+            else
+            {
+                System.out.println(Champions.showChampion(Champions.getChampion(champName)));
+            }
+
+
+        } while (doContinue);
+
     }
 }
