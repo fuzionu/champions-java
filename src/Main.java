e0782f4b670ee92ae57d48e5bf62aa3e4c663664
fuzionu <fuzionu@gmail.com>
2020-02-02 22:22:44 +0100

    moved Main code to TextInterface

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,8 @@
-import java.util.Scanner;
-
 public class Main
 {
     public static void main(String[] args)
     {
-        Scanner scanner = new Scanner(System.in);
-
         TextInterface textInterface = new TextInterface();
-
-        do
-        {
-            System.out.println("Type champion's name or \"exit\" to close the program");
-            System.out.print(">");
-            String userInput = scanner.nextLine();
-
-            if (userInput.equals("exit"))
-            {
-                System.out.println("Closing program...");
-                break;
-            }
-
-            try
-            {
-                Champion champion = ChampionRepository.getChampion(userInput);
-                System.out.println(textInterface.showChampion(champion));
-                System.out.println();
-            }
-            catch (ChampionNotFoundException e)
-            {
-                System.out.println("Champion not found! Try again.");
-            }
-
-        } while (true);
-
+        textInterface.start();
     }
 }
--- src/TextInterface.java
+++ src/TextInterface.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class TextInterface
 {
     public String showChampion(Champion champion)
@@ -17,4 +19,36 @@ public class TextInterface
                 "Magic resist: "   + champion.magicResist   + "\n" +
                 "Movement speed: "  + champion.movementSpeed;
     }
+
+    public void start()
+    {
+        Scanner scanner = new Scanner(System.in);
+        TextInterface textInterface = new TextInterface();
+
+        do
+        {
+            System.out.println("Type champion's name or \"exit\" to close the program");
+            System.out.print(">");
+            String userInput = scanner.nextLine();
+
+            if (userInput.equals("exit"))
+            {
+                System.out.println("Closing program...");
+                break;
+            }
+
+            try
+            {
+                Champion champion = ChampionRepository.getChampion(userInput);
+                System.out.println(textInterface.showChampion(champion));
+                System.out.println();
+            }
+            catch (ChampionNotFoundException e)
+            {
+                System.out.println("Champion not found! Try again.");
+            }
+
+        } while (true);
+    }
+
 }
