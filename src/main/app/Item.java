6b8f4644e1169735dc2c07841a7cea96e51e585a
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2020-02-18 17:28:03 +0100

    added classes Item and ItemRepository

---
+++ src/main/app/Item.java
@@ -0,0 +1,11 @@
+package app;
+
+import java.util.List;
+
+public class Item
+{
+    public String name;
+    public int price;
+    public String image;
+    public List<Item> subParts;
+}
---
+++ src/main/app/ItemRepository.java
@@ -0,0 +1,70 @@
+package app;
+
+import java.util.List;
+import java.util.Set;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.*;
+
+public class ItemRepository
+{
+    private Item item(String name, int price, String image, List<Item> subParts)
+    {
+        Item needlesslyLargeRod = new Item();
+        needlesslyLargeRod.name = name;
+        needlesslyLargeRod.price = price;
+        needlesslyLargeRod.image = image;
+        needlesslyLargeRod.subParts = subParts;
+        return needlesslyLargeRod;
+    }
+
+    public Set<Item> getItems()
+    {
+        Item item = getRabadon(getNeedlesslyLargeRod());
+
+        return singleton(item);
+    }
+
+    public Item getRabadon(Item subpart)
+    {
+        return item("Rabadon's Deathcap", 3600, "rabadon.png", asList(subpart, subpart));
+    }
+
+    public Item getBotRK()
+    {
+        return item("Blade of the Ruined King", 3600, "botrk.png",
+                asList(getRecurveBow(getDagger()), getBilgewaterCutlass(getVampiricScepter(getLongSword()), getLongSword())));
+    }
+
+    public Item getLongSword()
+    {
+
+        return item("Long Sword", 350, "longsword.png", emptyList());
+    }
+
+    public Item getNeedlesslyLargeRod()
+    {
+
+        return item("Nedlessly Large Rod", 1250, "largerod.png", emptyList());
+    }
+
+    public Item getDagger()
+    {
+        return item("Dagger", 300, "dagger.png", emptyList());
+    }
+
+    public Item getRecurveBow(Item dagger)
+    {
+        return item("Recurve Bow", 1000, "recurvebow.png", asList(dagger, dagger));
+    }
+
+    public Item getVampiricScepter(Item longSword)
+    {
+        return item("Vampiric Scepter", 900, "vampiricscepter.png", singletonList(longSword));
+    }
+
+    public Item getBilgewaterCutlass(Item vampiricScepter, Item longSword)
+    {
+        return item("Bilgewater Cutlass", 1600, "bilgewatercutlass.png", asList(vampiricScepter, longSword));
+    }
+}
